1f4804da08102d52d1a90dce5ec84e9e39478109
Maleknoura <maleknoura098@gmail.com>
2024-04-16 15:19:49 +0200

    singlepage

--- resources/views/singlepagebook.blade.php
+++ resources/views/singlepagebook.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +9,7 @@
     <link rel="stylesheet" href="{{ asset('css/actuality.css') }}">
     <title>Document</title>
 </head>
+
 <body>
     <!-- component -->
     <header class="header-area">
@@ -42,28 +44,48 @@
             </div>
         </div>
     </header>
-<div class="flex flex-col  justify-center mt-12">
-	<div
-		class="relative gap-8 flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
-		<div class="w-full md:w-1/3 bg-white grid place-items-center">
-			<img src="{{ $book->image }}" alt="" class="rounded-xl w-full" />
-    </div>
-			<div class="w-full md:w-1/2 bg-white flex flex-col space-y-2 p-3">
-				<div class="flex justify-between item-center">
-					<div class="flex items-center">
-						
-					
-					</div>
-					
-				
-				</div>
-				<h3 class="font-black text-gray-800 md:text-3xl text-xl">{{ $book->title }}</h3>
-				<p class="md:text-lg text-gray-500 text-base">{{ $book->content }}</p>
-				<p class="text-xl  text-gray-800">{{ $book->auteur }}</p>
-                <a href="{{ $book->pathpdf }}" class="text-blue-600 hover:underline">Télécharger le PDF</a>
+   
+    <div class="max-w-screen-lg mx-auto p-5 sm:p-10 md:p-16">
 
-			</div>
-		</div>
-	</div>
+        <div class="mb-10 rounded overflow-hidden flex flex-col mx-auto text-center">
+            <a href="#"
+                class="max-w-3xl mx-auto text-xl sm:text-4xl text-gray-400 inline-block hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2">{{ $book->title }}</a>
+    
+            <a href="#">
+                <img class="w-full h-80 my-4 "
+                    src="{{ $book->image }}"
+                    alt="">
+            </a>
+            <p class="text-gray-700 text-base leading-8 max-w-2xl mx-auto">
+                {{ $book->content }}
+            </p>
+            <div class="py-5 text-sm font-regular text-gray-900 flex items-center justify-center">
+                <span class="mr-3 flex flex-row items-center">
+                   
+                    
+                <a href="#" class="flex flex-row items-center hover:text-indigo-600  mr-3">
+                    <svg class="text-indigo-600" fill="currentColor" height="16px" aria-hidden="true" role="img"
+                        focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path fill="currentColor"
+                            d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z">
+                        </path>
+                        <path d="M0 0h24v24H0z" fill="none"></path>
+                    </svg>
+                    <span class="ml-1">{{ $book->auteur }}</span></a>
+                <a href="{{ $book->pathpdf }}" class="flex flex-row items-center hover:text-indigo-600">
+                    <svg class="text-indigo-600" fill="currentColor" height="16px" aria-hidden="true" role="img"
+                        focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
+                        <path fill=""
+                            d="M15.4496399,8.42490555 L8.66109799,1.63636364 L1.63636364,1.63636364 L1.63636364,8.66081885 L8.42522727,15.44178 C8.57869221,15.5954158 8.78693789,15.6817418 9.00409091,15.6817418 C9.22124393,15.6817418 9.42948961,15.5954158 9.58327627,15.4414581 L15.4486339,9.57610048 C15.7651495,9.25692435 15.7649133,8.74206554 15.4496399,8.42490555 Z M16.6084423,10.7304545 L10.7406818,16.59822 C10.280287,17.0591273 9.65554997,17.3181054 9.00409091,17.3181054 C8.35263185,17.3181054 7.72789481,17.0591273 7.26815877,16.5988788 L0.239976954,9.57887876 C0.0863319284,9.4254126 0,9.21716044 0,9 L0,0.818181818 C0,0.366312477 0.366312477,0 0.818181818,0 L9,0 C9.21699531,0 9.42510306,0.0862010512 9.57854191,0.239639906 L16.6084423,7.26954545 C17.5601275,8.22691012 17.5601275,9.77308988 16.6084423,10.7304545 Z M5,6 C4.44771525,6 4,5.55228475 4,5 C4,4.44771525 4.44771525,4 5,4 C5.55228475,4 6,4.44771525 6,5 C6,5.55228475 5.55228475,6 5,6 Z">
+                        </path>
+                    </svg>
+                    <span class="ml-1">Telecharger le pdf</span></a>
+            </div>
+            <hr>
+    
+        </div>
+    
+    </div>
 </body>
+
 </html>
